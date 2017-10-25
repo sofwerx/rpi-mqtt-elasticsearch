56b7fdddceb82234989033b34a5973ac6ae2c056
Ian Blenke <ian@blenke.com>
2017-10-25 08:15:13 -0400

    Fix syntax typo

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 . .env
 docker pull ${TRAVIS_REPO_SLUG}:latest || true
-docker build -t \
+docker build \
   --cache-from ${TRAVIS_REPO_SLUG}:latest \
-  ${TRAVIS_REPO_SLUG}:latest .
+  -t ${TRAVIS_REPO_SLUG}:latest .
