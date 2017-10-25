196b6f70ce1217172133006edb66abeb054ec55e
Ian Blenke <ian@blenke.com>
2017-10-25 08:05:28 -0400

    Adding cache-from for docker image build

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 . .env
 docker pull ${TRAVIS_REPO_SLUG}:latest || true
-docker build -t ${TRAVIS_REPO_SLUG}:latest .
+docker build -t \
+  --cache-from ${TRAVIS_REPO_SLUG}:latest \
+  ${TRAVIS_REPO_SLUG}:latest .
